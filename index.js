3eaca0d21faa3fd2d2cf839efc18031402428a11
Cameron Westland <camwest@gmail.com>
2012-11-04 10:10:38 -0500

    Added initial code

---
+++ index.js
@@ -0,0 +1,18 @@
+var GUID = function(){
+  var S4 = function ()
+  {
+    return Math.floor(
+      Math.random() * 0x10000 /* 65536 */
+    ).toString(16);
+  };
+
+  return (
+    S4() + S4() + "-" +
+    S4() + "-" +
+    S4() + "-" +
+    S4() + "-" +
+    S4() + S4() + S4()
+  );
+}
+
+module.exports = GUID;
